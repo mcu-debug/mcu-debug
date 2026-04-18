f7039965bfec39cf37f769b5cea2ca0489f28419
Haneef Mohammed <haneefdm@gmail.com>
2026-04-17 23:31:16 -0400

    Removed compiler output, added debug messages at startup

--- packages/shared/lib/command-exists.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function commandExists(commandName: string): boolean;
--- packages/shared/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export * from "./find-free-ports";
-export * from "./command-exists";
-export * from "./handles";
-export * from "./wait-for-port";
-export * from "./run-decoder";
-export * from "./proxy-network";
