e0437855f087063a71ae833a4b97f55c01c0ff59
Haneef Mohammed <haneefdm@gmail.com>
2026-07-27 11:11:58 -0400

    Update to new version

--- scripts/sync-versions.js
+++ scripts/sync-versions.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 // Edit VERSION here to update all packages in sync.
-const VERSION = "0.1.6";
+const VERSION = "0.1.7";
 
 const fs = require("fs");
 const path = require("path");
